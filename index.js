5f7f746d496e85cd44a4108bcccf4eb6e1018493
BuildTools <cpkhaled@yahoo.com>
2021-05-17 00:15:04 +0300

    First actual commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const Moonstone = require("moonstone-wrapper");
+const bot = Moonstone(process.env.API_KEY);
+
+bot.on("ready", async (user) => {
+  // Log to the console the username that was logged in as.
+  console.log("Ready! Logged in as " + user.username);
+
+  // Create a room then join it
+  const room = await bot.createRoom({
+    name: "DogeAwad Bot Room",
+    description: "The DogeAwad Bot Room. (Bot in development)",
+  });
+  await bot.joinRoom(room);
+});
+
+bot.on("handRaised", async (user, room) => {
+  if (room) {
+    await user.setAsSpeaker();
+  }
+});
+
+bot.on("userJoinRoom", async (user, room) => {
+  if (room) {
+    const welcomeMessage =
+      "Welcome to " +
+      room.name +
+      "! " +
+      "You can speak by requesting to speak.";
+
+    await user.sendWhisper(welcomeMessage);
+  }
+});
+
+// Connect the bot to Dogehouse
+bot.connect();
