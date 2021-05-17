7084c612e7ba4cb4ca331ffbe5d08b4a4329f904
BuildTools <cpkhaled@yahoo.com>
2021-05-17 19:50:53 +0300

    Added volume command and help msg

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const bot = Moonstone(process.env.API_KEY);
 const { playBackgroundMusic } = require("./commands");
 
 bot.on("ready", async (user) => {
-  // Log to the console the username that was logged in as.
   console.log("Ready! Logged in as " + user.username);
 
-  // Create a room then join it
   const room = await bot.createRoom({
     name: "DogeAwad Bot Room",
     description: "The DogeAwad Bot Room. (Bot in development)",
@@ -17,26 +15,70 @@ bot.on("ready", async (user) => {
 });
 
 bot.on("joinedRoom", async (room) => {
-  // play background music
   playBackgroundMusic(room);
 });
 
 bot.on("userJoinRoom", async (user, room) => {
   if (room) {
-    // welcome message
     const welcomeMessage =
       "Welcome to " +
       room.name +
-      "! " +
-      "Ask to speak if you want to. (I always have background music so that you can chill while speaking or listening)";
+      ". " +
+      "dw!help (I always have background music so that you can chill while speaking or listening)";
 
     await user.sendWhisper(welcomeMessage);
   }
 });
 
 bot.on("handRaised", async (user, room) => {
-  // set user as a speaker
-  await user.setAsSpeaker();
+  if (room) {
+    if (user.username === "cpkhaled") {
+      await user.setAsSpeaker();
+    }
+  }
+});
+
+bot.on("newChatMsg", async (msg) => {
+  if (msg.content.startsWith("dw!")) {
+    const command = msg.content.includes(" ")
+      ? msg.content.split(" ")[0]
+      : msg.content;
+
+    const args = msg.content.includes(" ")
+      ? msg.content.split(" ").slice(1)
+      : [];
+
+    const helpMsg =
+      "Commands: dw!msvl <volume> - Set the background music volume (0 - 2).";
+
+    switch (command) {
+      case "dw!help":
+        await msg.user.sendWhisper(helpMsg);
+        break;
+      case "dw!msvl":
+        if (!args) {
+          await msg.room.sendChatMessage("Invalid volume.");
+        } else {
+          const volume = parseInt(args[0]);
+
+          if (volume < 0 || volume > 2) {
+            await msg.room.sendChatMessage("Invalid volume.");
+          } else {
+            // TODO: Fix error > Cannot read property 'player' of undefined
+            const audioConnection = msg.room.audioConnection;
+            audioConnection.player.dispatcher.setVolume(args[0]);
+            await msg.room.sendChatMessage("Volume has been set to " + args[0]);
+            break;
+          }
+        }
+        break;
+      default:
+        await msg.room.sendChatMessage(
+          "Sorry, the command you sent is unknown."
+        );
+        break;
+    }
+  }
 });
 
 // Connect the bot to Dogehouse
